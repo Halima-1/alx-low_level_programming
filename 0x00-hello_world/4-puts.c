088bc10e6dcf40846612844eabe4fd046f093e6e
dahunsi alimot olajumoke <dahunsiolajumoke18@gmail.com>
2022-10-14 07:56:36 -0700

    no

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 /**
- * main- Serves as the entry point for the program
- * 
+ * main- Serves as the entry point for the program 
  * Return: By fault returns zero if no errors otherwise returns non-zero value.
- * */
+ **/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
