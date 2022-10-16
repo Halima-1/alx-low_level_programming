bbe301491bb6a6ecf2fc62d644c74d64cf3fa641
dahunsi alimot olajumoke <dahunsiolajumoke18@gmail.com>
2022-10-15 17:04:50 -0700

    commited

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+
 #include <stdlib.h>
+
 #include <time.h>
 /**
  * main-program entry point.
@@ -9,7 +11,7 @@ int main(void)
 {
 	int n;
 	srand(time(0));
-	n = rand()- RAND_MAX / 2;
+	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
@@ -22,5 +24,3 @@ int main(void)
 		printf("%d is zero\n", n);
 	return (0);
 }
-
-
