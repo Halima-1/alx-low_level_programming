911f5ed614963596da39859b97cdf7ba395bd7cb
dahunsi alimot olajumoke <dahunsiolajumoke18@gmail.com>
2022-10-23 07:08:59 -0700

    done

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,7 +3,7 @@
 /**
  * _isuper - checks if parameter is an upppercase character.
  * @c: input character.
- * Return: 1 if it is an uppercase character, 0 if not.
+ * Return: 1 if is an uppercase character, 0 in other case.
  */
 int _isupper(int c)
 {
@@ -16,4 +16,3 @@ int _isupper(int c)
 		return (0);
 	}
 }
-
