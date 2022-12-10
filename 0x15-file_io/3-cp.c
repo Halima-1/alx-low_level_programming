4d1cf6943c67c348d5c7b1f0df964add9a9bc42c
Alimot dahunsi <dahunsiolajumoke18@gmail.com>
2022-12-09 17:00:33 -0800

    done

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -23,4 +23,62 @@ char buff[1024];
 
 file_r = open(from, O_RDONLY);
 if (file_r == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", from);
+exit(98);
+}
+file_w = open(to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+if (file_w == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", to);
+exit(99);
+}
+r = read(file_r, buff, 1024);
+if (r == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", from);
+exit(98);
+}
+while (r != 0)
+{
+w = write(file_w, buff, r);
+if (w == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s", to);
+exit(99);
+}
+r = read(file_r, buff, 1024);
+if (r == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", from);
+exit(98);
+}
+}
+if (close(file_w) == -1)
+not_close(file_r);
+if (close(file_r) == -1)
+not_close(file_w);
+}
+/**
+ * main - copy a file into another
+ * @argc: size
+ * @argv: character.
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
 
+if (argc != 3)
+{
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+exit(97);
+}
+if (argv[1] == NULL)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+exit(98);
+}
+cp(argv[1], argv[2]);
+return (0);
+}
